7d6f9a7889e873ca6c9f9b0a1f9d71b23345f680
Abdul Samet Durmaz <abdulsametdurmaz54@gmail.com>
2022-03-04 20:48:30 +0300

    dronekit clone

---
+++ Core/Inc/cdkit.h
@@ -0,0 +1,25 @@
+/*
+ * cdkit.h
+ *
+ *  Created on: Mar 4, 2022
+ *      Author: abdul
+ */
+
+#ifndef INC_CDKIT_H_
+#define INC_CDKIT_H_
+
+
+
+void droneStop();
+
+void droneYawRight();
+void droneYawLeft();
+
+void dronePitchForward();
+void dronePitchBack();
+
+void droneRollRight();
+void droneRollLeft();
+
+
+#endif /* INC_CDKIT_H_ */
