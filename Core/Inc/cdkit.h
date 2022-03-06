831c7b1183a6ae277ff9e57013e68c6ff342fadb
durmaz54 <abdulsametdurmaz54@gmail.com>
2022-03-06 22:35:18 +0300

    autonomous uart protocol files was created

--- Core/Inc/cdkit.h
+++ Core/Inc/cdkit.h
@@ -8,18 +8,35 @@
 #ifndef INC_CDKIT_H_
 #define INC_CDKIT_H_
 
+#include "stm32f4xx.h"
+#include "stdint.h"
+
+#define CD_WHOAMI     0xCD
+#define CD_YAW        0x50
+#define CD_ROLL		0x51
+#define CD_PITCH		0x52
+#define CD_PLUS    	0x53
+
+void cdkit_read(UART_HandleTypeDef UARTx, int16_t *setpointYaw, int16_t *setpointPitch, int16_t *setpointRoll);
+
+void droneYawRight(int16_t *setpointYaw, int16_t *setpointPitch,
+		int16_t *setpointRoll);
 
 
-void droneStop();
 
-void droneYawRight();
-void droneYawLeft();
+void droneYawLeft(int16_t *setpointYaw, int16_t *setpointPitch,
+		int16_t *setpointRoll);
 
-void dronePitchForward();
-void dronePitchBack();
+void dronePitchForward(int16_t *setpointYaw, int16_t *setpointPitch,
+		int16_t *setpointRoll);
+void dronePitchBack(int16_t *setpointYaw, int16_t *setpointPitch,
+		int16_t *setpointRoll);
 
-void droneRollRight();
-void droneRollLeft();
+void droneRollRight(int16_t *setpointYaw, int16_t *setpointPitch,
+		int16_t *setpointRoll);
+void droneRollLeft(int16_t *setpointYaw, int16_t *setpointPitch,
+		int16_t *setpointRoll);
+
 
 
 #endif /* INC_CDKIT_H_ */
--- Core/Src/cdkit.c
+++ Core/Src/cdkit.c
@@ -5,4 +5,65 @@
  *      Author: abdul
  */
 
+#include "cdkit.h"
 
+void cdkit_read(UART_HandleTypeDef UARTx, int16_t *setpointYaw,
+		int16_t *setpointPitch, int16_t *setpointRoll) {
+	char data[5];
+
+	HAL_UART_Receive(&UARTx, data, 4, 50);
+
+	if ((data[0] == CD_WHOAMI) & ((data[1] + data[2]) == data[3])) {
+
+		switch (data[1]) {
+		case CD_PITCH:
+			if(data[2] == CD_PLUS){
+				//dronePitchForward(setpointYaw, setpointPitch, setpointRoll);
+				*setpointPitch = 100;
+			}
+			else{
+				//dronePitchBack(setpointYaw, setpointPitch, setpointRoll);
+			}
+			break;
+		case CD_ROLL:
+			if(data[2] == CD_PLUS){
+				//droneRollRight(setpointYaw, setpointPitch, setpointRoll);
+			}else{
+				//droneRollLeft(setpointYaw, setpointPitch, setpointRoll);
+			}
+
+			break;
+		case CD_YAW:
+			if(data[2] == CD_PLUS){
+				//droneYawRight(setpointYaw, setpointPitch, setpointRoll);
+			}else{
+				//droneYawLeft(setpointYaw, setpointPitch, setpointRoll);
+			}
+			break;
+		default:
+			break;
+		}
+	}
+
+}
+
+void droneYawRight(int16_t *setpointYaw, int16_t *setpointPitch,
+		int16_t *setpointRoll){
+
+	*setpointYaw = 5;
+
+}
+void droneYawLeft(int16_t *setpointYaw, int16_t *setpointPitch,
+		int16_t *setpointRoll);
+
+void dronePitchForward(int16_t *setpointYaw, int16_t *setpointPitch,
+		int16_t *setpointRoll);
+
+
+void dronePitchBack(int16_t *setpointYaw, int16_t *setpointPitch,
+		int16_t *setpointRoll);
+
+void droneRollRight(int16_t *setpointYaw, int16_t *setpointPitch,
+		int16_t *setpointRoll);
+void droneRollLeft(int16_t *setpointYaw, int16_t *setpointPitch,
+		int16_t *setpointRoll);
